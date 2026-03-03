8f974c5ff0b32af9682416836ebcded9e9f5fdab
johnpaul <edehjohnpaul@gmail.com>
2026-03-03 13:30:58 +0100

    fix: remove dist from git, add postinstall prisma generate, add dist to gitignore

--- dist/modules/trades/trade.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.quoteTrade = quoteTrade;
-const trade_service_1 = require("./trade.service");
-async function quoteTrade(req, res) {
-    const { tradeId } = req.params;
-    await (0, trade_service_1.updateTradeStatus)(tradeId, "QUOTED", req.user);
-    res.json({ success: true });
-}
